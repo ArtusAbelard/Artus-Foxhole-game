8f436be814a91dfd53903e8365beb57a25607d90
ArtusAbelard <tribelha@gmail.com>
2024-01-25 20:58:04 +0100

    info

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,39 @@ let acceuil = document.querySelector("#acceuil")
 let jeuxcolo = document.querySelector("#jeuxcolo")
 let jeuxwarden = document.querySelector("#jeuxwarden")
 let btnacceuil = document.querySelectorAll(".btnacceuil")
+let infobtn = document.querySelector("#infobtn")
+let infobtnwar = document.querySelector("#infobtnwar")
+let infocolo = document.querySelector(".infocolo")
+let infowarden = document.querySelector(".infowarden")
+let infobacktoacceuil = document.querySelectorAll(".backtoacceuil")
+let backtoacceuilwar = document.querySelector("#backtoacceuilwar")
 
 
 
 gifchoix[0].addEventListener('click', ()=>{
-    // alert("ewa")
     acceuil.style.display="none"
     jeuxcolo.style.display="block"
 })
 gifchoix[1].addEventListener('click', ()=>{
     acceuil.style.display="none"
     jeuxwarden.style.display="block"
+})
+
+
+infobtn.addEventListener('click', ()=>{
+    infocolo.style.display="flex"
+
+})
+infobtnwar.addEventListener("click", ()=>{
+    infowarden.style.display="flex"
+    
+})
+infobacktoacceuil[0].addEventListener("click",()=>{
+    infocolo.style.display="none"
+})
+backtoacceuilwar.addEventListener('click', ()=>{
+    infowarden.style.display="none"
+
 })
 
 for (let index = 0; index < btnacceuil.length; index++) {
